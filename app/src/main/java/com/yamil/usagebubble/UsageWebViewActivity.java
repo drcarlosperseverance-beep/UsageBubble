f6414c9ad0b13ba6aac7195721e4013d9383f5dd
drcarlosperseverance-beep <drcarlosperseverance-beep@users.noreply.github.com>
2026-09-06 01:25:48 -0400

    Corregir importacion de Intent

--- app/src/main/java/com/yamil/usagebubble/UsageWebViewActivity.java
+++ app/src/main/java/com/yamil/usagebubble/UsageWebViewActivity.java
@@ -1,6 +1,7 @@
 package com.yamil.usagebubble;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
